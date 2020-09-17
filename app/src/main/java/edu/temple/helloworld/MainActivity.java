dc3cdbcc7ed1748861624c30092906d0e02687e5
ChenxuanSun <tug81816@temple.edu>
2020-09-17 14:56:28 +0800

    I put a button in my xml file and named:"MyfirstButton", in my MainActivity.java file, I use findViewById to find my button.and then I write a "setOnclickListener" from the button, And inside this I "new a View.OnclickListener". After that I have to override onClick method. I use findViewById to find My own Textview, and I use setText method to change the text, When I click the button, the TextView will change.

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,12 +3,29 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.MyfirstButton);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+               TextView view1 = (TextView) findViewById(R.id.textView);
+               view1.setText("You have clicked the button");
+            }
+        });
     }
 }
